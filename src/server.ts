5789c88a1e2d931a4404f842bed54021212b0c77
Kristián Duda <kristian.duda@extropy.sk>
2023-11-23 21:39:57 +0100

    add collections

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import express from 'express'
-import payload from 'payload'
+import express from "express";
+import payload from "payload";
+import { seed } from "./seed";
 
-require('dotenv').config()
-const app = express()
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Redirect root to Admin panel
-app.get('/', (_, res) => {
-  res.redirect('/admin')
-})
+app.get("/", (_, res) => {
+  res.redirect("/admin");
+});
 
 const start = async () => {
   // Initialize Payload
@@ -15,13 +17,18 @@ const start = async () => {
     secret: process.env.PAYLOAD_SECRET,
     express: app,
     onInit: async () => {
-      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
     },
-  })
+  });
 
-  // Add your own express routes here
+  if (process.env.PAYLOAD_SEED === "true") {
+    await seed(payload);
+    process.exit();
+  }
 
-  app.listen(3000)
-}
+  app.listen(PORT, async () => {
+    payload.logger.info(`App URL: ${process.env.PAYLOAD_PUBLIC_SERVER_URL}`);
+  });
+};
 
-start()
+start();
